b70a857073b90103cc70ccde9be9505dbef8f99e
elpaiba <andybenjaminpaibavasquez@gmail.com>
2026-06-04 00:03:36 -0500

    Agregando ejercicios del 1 al 3

---
+++ EJERCICIOS 1-3/EJERCICIO 1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace EJERCICIO_1
+{
+    class Program
+    {
+        static double CalcularSalario(double valorHora, int horas)
+        {
+            if (horas <= 192)
+            {
+                return valorHora * horas;
+            }
+            else
+            {
+                int horasExtra = horas - 192;
+                double pagoNormal = valorHora * 192;
+                double pagoExtra = horasExtra * valorHora * 1.50;
+                return pagoNormal + pagoExtra;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== PLANILLA INDUSTRIAL ===");
+            Console.Write("Cantidad de obreros: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double totalPlanilla = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"\nObrero {i}");
+                Console.Write("Valor por hora: S/");
+                double vh = double.Parse(Console.ReadLine());
+                Console.Write("Horas trabajadas: ");
+                int hmt = int.Parse(Console.ReadLine());
+
+                double salario = CalcularSalario(vh, hmt);
+                totalPlanilla += salario;
+
+                Console.WriteLine($"Salario: S/{salario:F2}");
+            }
+
+            Console.WriteLine($"\nTotal de planilla: S/{totalPlanilla:F2}");
+        }
+    }
+}
+    
+    
+   
---
+++ EJERCICIOS 1-3/Ejercicio 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static double CalcularImporte(int cantidad, double precio)
+        {
+            return cantidad * precio;
+        }
+
+        static void MostrarPedido(string plato, int cantidad, double importe)
+        {
+            Console.WriteLine($"Pedido: {plato} | Cantidad: {cantidad} | Importe: S/{importe:F2}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== CONTROL DE PEDIDOS DEL DÍA ===");
+            double totalDia = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"\nPedido {i}");
+                Console.Write("Nombre del plato: ");
+                string plato = Console.ReadLine();
+                Console.Write("Cantidad: ");
+                int cantidad = int.Parse(Console.ReadLine());
+                Console.Write("Precio unitario: S/");
+                double precio = double.Parse(Console.ReadLine());
+
+                double importe = CalcularImporte(cantidad, precio);
+                totalDia += importe;
+
+                MostrarPedido(plato, cantidad, importe);
+            }
+
+            Console.WriteLine($"\nTotal vendido del día: S/{totalDia:F2}");
+        }
+    }
+}
+
